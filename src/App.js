40ca9f9e6e23cb347192743c54359cb3c2649c7b
meof-coding <tranthe150186@fpt.edu.vn>
2022-02-16 17:19:02 +0700

    comit react three fiber

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 // import logo from './logo.svg';
-// import './App.css';
-// import ReactDOM from 'react-dom'
+import './App.css';
+import {useState} from 'react'
 // import { Physics, useCylinder, usePlane } from '@react-three/cannon'
-import { Canvas } from '@react-three/fiber'
+import DATGUI from 'react-dat-gui'
+import { Canvas, render, events } from '@react-three/fiber'
 import Ground from './component/Ground.js'
-import { OrbitControls, Environment } from '@react-three/drei'
-// import { PlaneBufferGeometry } from 'three';
+import { OrbitControls, TransformControls } from '@react-three/drei'
+
+
 function App() {
+  console.log(DATGUI);
+  const [size, setSize] = useState({width:window.innerWidth, height:window.innerHeight});
+  window.addEventListener('resize',()=>{
+    setSize({width:window.innerWidth, height:window.innerHeight})
+  })
+
+  
   return (
-    <div id="canvas-container">
-      <Canvas dpr={[1, 1.5]} shadows camera={{ position: [0, 5, 15], fov: 50 }}>
+    <div id="canvas-container" style = {{width: size.width, height: size.height}}>
+      <Canvas pixelRatio={window.devicePixelRatio} dpr={[1, 1.5]} shadows camera={{ position: [0, 0, 3], fov: 50 }} render={{ scroll: true, debounce: { scroll: 50, resize: 1}}} >
         <fog attach="fog" args={['#171720', 10, 50]} />
-        <Ground />
+        <Ground/>
+
         <OrbitControls />
       </Canvas>
 
--- src/component/Ground.js
+++ src/component/Ground.js
@@ -1,7 +1,7 @@
 export default function Ground() {
   return (
-      <mesh>
-          <planeGeometry args={[100,100]}/>
+      <mesh >
+          <planeGeometry args={[1,1]} position={[1, -2, 0]} rotation={[0,-Math.PI/4,0]} />
           <meshStandardMaterial color="#303030" />
       </mesh>
   ) 
